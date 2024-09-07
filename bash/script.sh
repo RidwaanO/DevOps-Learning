906d15f6007e8c21ad6a0624c8bda6af9e001ac2
RidwaanO <rao5@hw.ac.uk>
2024-09-07 18:36:37 +0100

    creating a bash file to play around with

---
+++ bash/script.sh
@@ -0,0 +1,9 @@
+script.sh
+#!/bin/bash
+length=5
+width=10
+
+result=$((length * width))
+perimeter=$((2 * (length+width)))
+
+echo " the area of the rectangle is $result while the perimeter is $perimeter"
