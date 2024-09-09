f03ac6ba8e6eb722148c6753aee1dc1301e8dbb9
RidwaanO <rao5@hw.ac.uk>
2024-09-09 01:06:56 +0100

    playing around with different bash scripts

--- bash/script.sh
+++ bash/script.sh
@@ -1,9 +1,29 @@
-script.sh
 #!/bin/bash
-length=5
-width=10
 
-result=$((length * width))
-perimeter=$((2 * (length+width)))
 
-echo " the area of the rectangle is $result while the perimeter is $perimeter"
+validate_age(){
+
+    if [[! $age =~ ^[0-9]+$ ]]
+    then
+        echo "Invalid input, please try again."
+        return 1
+    elif (($age<18))
+    then
+        echo "You are not eligible."
+        return 1
+    else
+        echo "You are eligible."
+        return 0
+    fi
+
+}
+
+echo "Please enter your age:"
+
+read age
+
+validate_age age
+
+exit_code = $?
+
+if [ $exit_code -nq 0 ]
