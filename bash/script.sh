41aff8e91760c49753d885094108fde1c059d028
RidwaanO <rao5@hw.ac.uk>
2024-09-14 15:13:20 +0100

    playing around with bash syntax

--- bash/script.sh
+++ bash/script.sh
@@ -1,29 +1,6 @@
 #!/bin/bash
 
-
-validate_age(){
-
-    if [[! $age =~ ^[0-9]+$ ]]
-    then
-        echo "Invalid input, please try again."
-        return 1
-    elif (($age<18))
-    then
-        echo "You are not eligible."
-        return 1
-    else
-        echo "You are eligible."
-        return 0
-    fi
-
-}
-
-echo "Please enter your age:"
-
-read age
-
-validate_age age
-
-exit_code = $?
-
-if [ $exit_code -nq 0 ]
+set -x
+echo "this is a test"
+X=10
+echo "X is equal to 10"
